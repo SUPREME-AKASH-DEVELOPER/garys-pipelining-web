377c99efd818fbaf12c389876ce18a4322d1235c
Akash Lakhwan <142366831+SUPREME-AKASH-DEVELOPER@users.noreply.github.com>
2026-06-26 22:32:38 +0530

    Delete garys-pipelining-web directory

--- garys-pipelining-web/src/components/ui/container.tsx
+++
@@ -1,3 +0,0 @@
-export function Container({ className = "", children }: { className?: string; children: React.ReactNode }) {
-  return <div className={`container-px mx-auto max-w-[1400px] ${className}`}>{children}</div>;
-}
